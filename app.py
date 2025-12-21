0c53a53f1cb0a5f476f1452260ec25ed96808e91
PENTAKOTA BHAVISHYA <bhavishyapentakota@gmail.com>
2025-12-22 01:47:30 +0530

    Modify register route to use POST method only
    Change register route to accept only POST requests.

--- app.py
+++ app.py
@@ -60,7 +60,7 @@ def login():
     return render_template("login.html")
 
 
-@app.route("/register", methods=["GET", "POST"])
+@app.route("/register", methods=["POST"])
 def register():
     if request.method == "POST":
         username = request.form.get("username")
